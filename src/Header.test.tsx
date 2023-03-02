d82f380184a27f789ff5583278f09e0e091cfec2
Ilya Shestakov <siavol@gmail.com>
2023-03-02 23:51:38 +0200

    render header

---
+++ src/Header.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import Header from './Header';
+
+test('renders my name', () => {
+    render(<Header />);
+    const linkElement = screen.getByText(/Ilya Shestakov/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('renders description', () => {
+    render(<Header />);
+    const linkElement = screen.getByText(/full stack developer/i);
+    expect(linkElement).toBeInTheDocument();
+});
