d83c0f7d22f9a1d18ebd3a98d64c2927d79d6d33
Ilya Shestakov <siavol@gmail.com>
2023-03-04 12:26:54 +0200

    Prettify

--- src/Header.test.tsx
+++ src/Header.test.tsx
@@ -3,13 +3,13 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 test('renders my name', () => {
-    render(<Header />);
-    const linkElement = screen.getByText(/Ilya Shestakov/i);
-    expect(linkElement).toBeInTheDocument();
+  render(<Header />);
+  const linkElement = screen.getByText(/Ilya Shestakov/i);
+  expect(linkElement).toBeInTheDocument();
 });
 
 test('renders description', () => {
-    render(<Header />);
-    const linkElement = screen.getByText(/full stack developer/i);
-    expect(linkElement).toBeInTheDocument();
+  render(<Header />);
+  const linkElement = screen.getByText(/full stack developer/i);
+  expect(linkElement).toBeInTheDocument();
 });
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
 function Header() {
-    return (
-        <div className="title row justify-content-center gy-5">
-        <div className="col-sm-8">
-            <h1 className="display-3">Ilya Shestakov</h1>
-            <small className="text-muted">Full stack developer with over than 15 years of experience</small>
-        </div>
-        {/* <div className="col-sm-1 align-self-end">
+  return (
+    <div className="title row justify-content-center gy-5">
+      <div className="col-sm-8">
+        <h1 className="display-3">Ilya Shestakov</h1>
+        <small className="text-muted">
+          Full stack developer with over than 15 years of experience
+        </small>
+      </div>
+      {/* <div className="col-sm-1 align-self-end">
             <a className="btn btn-sm btn-outline-primary" href="./Ilya-Shestakov-Resume.pdf" target="_blank" rel="noopener noreferrer">.pdf</a>
         </div> */}
     </div>
-    );
+  );
 }
 
 export default Header;
